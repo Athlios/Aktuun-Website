49fab727309039ec1d582292b7912bdcb0ea49d2
Athlios <93403057+Athlios@users.noreply.github.com>
2021-12-18 19:35:46 +0000

    Changes

--- MainWebsite/Code/script.js
+++ MainWebsite/Code/script.js
@@ -1,10 +1,7 @@
-window.addEventListener('scroll', function() {
-		var element = document.querySelector('#main-container');
-		var position = element.getBoundingClientRect();
-
-		if(position.top >= 0 && position.bottom <= window.innerHeight) {
-			document.getElementById('target-id').style.disaply = "show";
-		}
-		if(position.top < window.innerHeight && position.bottom >= 0) {
-			document.getElementById('target-id').style.disaply = "none";	
-		}
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 200) {
+		$('.fixelement').css({
+			'display': 'none'
+		});
+	}
+});
